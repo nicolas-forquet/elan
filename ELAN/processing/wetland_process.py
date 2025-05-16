cd9229f59f206de981c0d778454e15282c9095c1
Jacky Volpes <jacky.volpes@oslandia.com>
2025-05-16 15:52:48 +0200

    fix(wetland-process): tkn can be null

--- ELAN/processing/wetland_process.py
+++ ELAN/processing/wetland_process.py
@@ -369,14 +369,12 @@ class WetlandProcessAlgorithm(QgsProcessingAlgorithm, Translatable):
             BOD5_obj = wwtp_feature[BOD5_obj_field_name]
             if BOD5_obj == NULL:
                 obj_none.append(self.tr("BOD5"))
-            TKN_obj = wwtp_feature[TKN_obj_field_name]
-            if TKN_obj == NULL:
-                obj_none.append(self.tr("TKN"))
             COD_obj = wwtp_feature[COD_obj_field_name]
             if COD_obj == NULL:
                 obj_none.append(self.tr("COD"))
             if len(obj_none) > 0:
                 raise QgsProcessingException(self.tr("These values can't be NULL:") + " " + ", ".join(obj_none))
+            TKN_obj = wwtp_feature[TKN_obj_field_name]
             NO3_obj = wwtp_feature[NO3_obj_field_name]
             TN_obj = wwtp_feature[TN_obj_field_name]
             Q = wwtp_feature[Q_obj_field_name]
