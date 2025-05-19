bf5a294bfa89d91cfb50f060c88729366547f5b1
Sophie Aubier <sophie.aubier@oslandia.com>
2025-05-19 18:23:04 +0200

    update : test_population

--- tests/test_population.py
+++ tests/test_population.py
@@ -4,44 +4,34 @@ test_population
 
 from pathlib import Path
 
-from qgis.core import QgsApplication, QgsVectorLayer
-from qgis.gui import QgisInterface
-
-DIR_PLUGIN_ROOT = Path(__file__).parent
+from ELAN.__about__ import DIR_PLUGIN_ROOT
+from tests.utils import assert_same_layers, load_layer
 
 
-def test_population(qgis_processing, mocker):
-    mocker.patch("ELAN.toolbelt.log_handler.iface", spec=QgisInterface)
+def test_population(qgis_processing, mocker, tmp_path):
     import processing
 
+    from ELAN.processing.population import PopulationAlgorithm
     from ELAN.processing.provider import ELANProvider
 
-    provider = ELANProvider()
-    if (registry := QgsApplication.processingRegistry()) is None:
-        raise RuntimeError("Processing registry not found")
-    registry.addProvider(provider)
+    mocker.patch("ELAN.utils.tr.PlgLogger")  # don't care about logging anything from translations
+    test_data_dir = Path(DIR_PLUGIN_ROOT).parent / "tests" / "data_test" / "population"
+
+    test_population_alg = PopulationAlgorithm()
+    assert test_population_alg.name() == "elanpopulation"
+    assert test_population_alg.groupId() == "elanpreprocessings"
 
     population_param = {
-        'INPUT_POPULATION_TOT':1100,
-        "INPUT_POLYGON_LAYER": f"{DIR_PLUGIN_ROOT}/data_test/population/population_input.gpkg.zip",
-        "OUPUT_CENTROIDES_LAYER": f"{DIR_PLUGIN_ROOT}/data_test/population/population_generated_output.gpkg",
+        "INPUT_POPULATION_TOT": 1100,
+        "INPUT_POLYGON_LAYER": str(test_data_dir / "population_input.gpkg.zip"),
+        "OUPUT_CENTROIDES_LAYER": str(tmp_path / "population_generated_output.gpkg"),
     }
-    processing.run("elan:elanpopulation", population_param)
+    res = processing.run(test_population_alg, population_param)
+    assert res != {}
 
-    ref_path = f"{DIR_PLUGIN_ROOT}/data_test/population/population_reference_output.gpkg.zip"
-    gen_path = f"{DIR_PLUGIN_ROOT}/data_test/population/population_generated_output.gpkg"
+    ref_path = str(test_data_dir / "population_reference_output.gpkg.zip")
+    gen_path = str(tmp_path / "population_generated_output.gpkg")
 
-    uri_ref = f"{ref_path}|layername=population_reference_output"
-    layer_ref = QgsVectorLayer(uri_ref, "population_reference_output", "ogr")
-    uri_gen = f"{gen_path}|layername=population_generated_output"
-    layer_gen = QgsVectorLayer(uri_gen, "population_generated_output", "ogr")
-
-    assert layer_gen.isValid()
-    assert layer_ref.isValid()
-
-    assert layer_ref.featureCount() == layer_gen.featureCount()
-    assert [field.name() for field in layer_ref.fields()] == [field.name() for field in layer_gen.fields()]
-
-
-
- 
+    assert_same_layers(
+        load_layer(ref_path, "population_reference_output"), load_layer(gen_path, "population_generated_output")
+    )
