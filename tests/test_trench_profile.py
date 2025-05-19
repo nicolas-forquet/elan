4629ec80fb88b4f7c839a1d3d80cc2af7ee5be7f
Jacky Volpes <jacky.volpes@oslandia.com>
2025-05-19 11:06:36 +0200

    update(test_trench_profile): use tmp_path fixture

--- tests/test_trench_profile.py
+++ tests/test_trench_profile.py
@@ -9,7 +9,7 @@ from ELAN.processing.trench_profile import TrenchProfileAlgorithm
 from tests.utils import assert_same_layers, load_layer
 
 
-def test_trench_profile(qgis_processing, mocker):
+def test_trench_profile(qgis_processing, mocker, tmp_path):
     import processing
 
     mocker.patch("ELAN.utils.tr.PlgLogger")  # don't care about logging anything from translations
@@ -22,14 +22,14 @@ def test_trench_profile(qgis_processing, mocker):
 
     trench_profile_param = {
         "INPUT_LAYER": str(test_data_dir / "trench_profile_input.gpkg.zip"),
-        "OUTPUT_LAYER": str(test_data_dir / "trench_profile_generated_output.gpkg"),
+        "OUTPUT_LAYER": str(tmp_path / "trench_profile_generated_output.gpkg"),
     }
 
     res = processing.run(trench_profile_alg, trench_profile_param)
     assert res == {}
 
     ref_path = str(test_data_dir / "trench_profile_reference_output.gpkg.zip")
-    gen_path = str(test_data_dir / "trench_profile_generated_output.gpkg")
+    gen_path = str(tmp_path / "trench_profile_generated_output.gpkg")
     layers = ["sewer_profile", "land_profile", "3D_pipes"]
 
     for name in layers:
