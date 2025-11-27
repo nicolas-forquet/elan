14ac9de327bb7b7640ee7f066992a3b4d71f470d
Jacky Volpes <jacky.volpes@oslandia.com>
2025-11-27 16:39:19 +0100

    feat(wetland-process): add test with surface

--- tests/test_wetland_process.py
+++ tests/test_wetland_process.py
@@ -13,7 +13,49 @@ from ELAN.__about__ import DIR_PLUGIN_ROOT
 from tests.utils import assert_same_layers, load_layer
 
 
+def test_wetland_process(elan_processing, tmp_path):
+    """Test wetland process with all inputs"""
+
+    from ELAN.processing.wetland_process import WetlandProcessAlgorithm
+
+    test_data_dir = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "wetland_process"
+    test_wetland_process_alg = WetlandProcessAlgorithm()
+    assert test_wetland_process_alg.name() == "elanwetlandprocess"
+    assert test_wetland_process_alg.groupId() == "elanprocessings"
+
+    wetland_process_param = {
+        "CLIMATE": 0,
+        "TREATMENT": str(tmp_path / "wetland_process_generated_output.gpkg"),
+        "SINKS": str(test_data_dir / "wetland_process_steu_input.gpkg.zip"),
+        "AVAILABLE_SURFACE": str(test_data_dir / "wetland_process_surface_input.gpkg.zip"),
+        "SINK_COORDS": "sink_coords",
+        "STAGES_MAX": 3,
+        "TSS_OBJ": "TSS_obj",
+        "BOD5_OBJ": "BOD5_obj",
+        "TKN_OBJ": "TKN_obj",
+        "COD_OBJ": "COD_obj",
+        "NO3N_OBJ": "NO3N_obj",
+        "TN_OBJ": "TN_obj",
+        "TP_OBJ": "TP_obj",
+        "ECOLI_OBJ": "ecoli_obj",
+        "Q_FIELD": "average_daily_flow",
+    }
+
+    res = elan_processing.run(test_wetland_process_alg, wetland_process_param)
+    assert list(res.keys()) == ["TREATMENT"]
+
+    ref_path = test_data_dir / "wetland_process_reference_output.gpkg.zip"
+    gen_path = tmp_path / "wetland_process_generated_output.gpkg"
+
+    assert_same_layers(
+        load_layer(ref_path, "couche_de_filires"),
+        load_layer(gen_path, "wetland_process_generated_output"),
+        check_values_only_on_fields=["fid", "sink_coords", "pathway_id", "available_surface", "name_stages"],
+    )
+
+
 def test_wetland_process_without_surface(elan_processing, tmp_path):
+    """Test wetland process without surface input layer"""
 
     from ELAN.processing.wetland_process import WetlandProcessAlgorithm
 
@@ -43,7 +85,7 @@ def test_wetland_process_without_surface(elan_processing, tmp_path):
     res = elan_processing.run(test_wetland_process_alg, wetland_process_param)
     assert list(res.keys()) == ["TREATMENT"]
 
-    ref_path = test_data_dir / "wetland_process_reference_output.gpkg.zip"
+    ref_path = test_data_dir / "wetland_process_reference_output_without_surface.gpkg.zip"
     gen_path = tmp_path / "wetland_process_generated_output.gpkg"
 
     assert_same_layers(
@@ -54,6 +96,7 @@ def test_wetland_process_without_surface(elan_processing, tmp_path):
 
 
 def test_wetland_process_with_negative_objectives(elan_processing, tmp_path):
+    """Test wetland process crashes when negative values are in target concentrations"""
 
     from ELAN.processing.wetland_process import WetlandProcessAlgorithm
 
@@ -85,6 +128,7 @@ def test_wetland_process_with_negative_objectives(elan_processing, tmp_path):
 
 
 def test_wetland_process_with_null_objectives(elan_processing, tmp_path):
+    """Test wetland process crashes when NULL values are in target concentrations"""
 
     from ELAN.processing.wetland_process import WetlandProcessAlgorithm
 
@@ -116,6 +160,10 @@ def test_wetland_process_with_null_objectives(elan_processing, tmp_path):
 
 
 def test_wetland_process_with_bad_inflow_concentrations(elan_processing, tmp_path):
+    """
+    Test wetland process crashes when there is bad values in inflow concentrations
+    that trigger an error in wetlandoptimizer
+    """
 
     from ELAN.processing.wetland_process import WetlandProcessAlgorithm
 
