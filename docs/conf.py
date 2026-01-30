e16369f6aeab2c23314ecf3975d938c491b71e44
Gabrielle FAVREAU <gabrielle.favreau@inrae.fr>
2026-01-30 11:38:30 +0100

    reset theme for mermaid diagramms to enable again zooming in

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,6 @@ extensions = [
     'sphinxcontrib.mermaid'
 ]
 
-mermaid_params = ['--theme', 'neutral']
-
 templates_path = ["_templates"]
 exclude_patterns = []
 figure_language_filename = '{root}.{language}{ext}'
