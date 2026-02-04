0fdddbfe411b083ddf3cc9c2eb93f3c8b4729aed
Gabrielle FAVREAU <gabrielle.favreau@inrae.fr>
2026-02-04 14:12:18 +0100

    change conf.py (test)

--- docs/conf.py
+++ docs/conf.py
@@ -27,11 +27,7 @@ language = "fr"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinxcontrib.mermaid'
-]
-
-mermaid_params = ['configApi.reset()']
+extensions = []
 
 templates_path = ["_templates"]
 exclude_patterns = []
