f0ea48f5af657e3adacc34ef8fba85c37347af8a
Sophie Aubier <sophie.aubier@oslandia.com>
2026-04-10 16:49:47 +0200

    review

--- ELAN/processing/wetland_process.py
+++ ELAN/processing/wetland_process.py
@@ -386,11 +386,11 @@ class WetlandProcessAlgorithm(QgsProcessingAlgorithm, Translatable):
                 output_feature["pathway_id"] = pathway_result.id
                 output_feature["available_surface"] = ceil(optimization_result.available_surface)
                 output_feature["sink_coords"] = wwtp_feature[sink_coords_field_name]
-                output_feature["total_volume"] = round(pathway_result.total_volume, 0)
+                output_feature["total_volume"] = round(pathway_result.total_volume)
                 output_feature["total_surface"] = ceil(pathway_result.total_surface)
                 output_feature["depth_stages_unsat"] = json.dumps(pathway_result.unsaturated_depth_stages)
                 output_feature["depth_stages_sat"] = json.dumps(pathway_result.saturated_depth_stages)
-                output_feature["surface_stages"] = json.dumps(ceil(pathway_result.surface_stages[0]))
+                output_feature["surface_stages"] = json.dumps([ceil(val) for val in pathway_result.surface_stages])
                 output_feature["TSS_loading_stages"] = json.dumps(pathway_result.TSS_loading_stages)
                 output_feature["BOD5_loading_stages"] = json.dumps(pathway_result.BOD5_loading_stages)
                 output_feature["TKN_loading_stages"] = json.dumps(pathway_result.TKN_loading_stages)
@@ -398,14 +398,14 @@ class WetlandProcessAlgorithm(QgsProcessingAlgorithm, Translatable):
                 output_feature["hydraulic_loading_rate_stages"] = json.dumps(
                     pathway_result.hydraulic_loading_rate_stages
                 )
-                output_feature["TSS_concentration"] = round(pathway_result.TSS_concentration, 0)
-                output_feature["BOD5_concentration"] = round(pathway_result.BOD5_concentration, 0)
-                output_feature["TKN_concentration"] = round(pathway_result.TKN_concentration, 0)
-                output_feature["COD_concentration"] = round(pathway_result.COD_concentration, 0)
-                output_feature["NO3N_concentration"] = round(pathway_result.NO3N_concentration, 0)
-                output_feature["TN_concentration"] = round(pathway_result.TN_concentration, 0)
-                output_feature["TP_concentration"] = round(pathway_result.TP_concentration, 0)
-                output_feature["ecoli_concentration"] = round(pathway_result.ecoli_concentration, 0)
+                output_feature["TSS_concentration"] = round(pathway_result.TSS_concentration)
+                output_feature["BOD5_concentration"] = round(pathway_result.BOD5_concentration)
+                output_feature["TKN_concentration"] = round(pathway_result.TKN_concentration)
+                output_feature["COD_concentration"] = round(pathway_result.COD_concentration)
+                output_feature["NO3N_concentration"] = round(pathway_result.NO3N_concentration)
+                output_feature["TN_concentration"] = round(pathway_result.TN_concentration)
+                output_feature["TP_concentration"] = round(pathway_result.TP_concentration)
+                output_feature["ecoli_concentration"] = round(pathway_result.ecoli_concentration)
                 output_feature["TSS_deviation"] = pathway_result.TSS_deviation
                 output_feature["BOD5_deviation"] = pathway_result.BOD5_deviation
                 output_feature["TKN_deviation"] = pathway_result.TKN_deviation
@@ -417,11 +417,11 @@ class WetlandProcessAlgorithm(QgsProcessingAlgorithm, Translatable):
 
                 # Normalized concentration values
                 TSS_obj, BOD5_obj, TKN_obj, COD_obj, NO3N_obj, TN_obj, TP_obj, ecoli_obj = optimization_result.Cobj
-                if TSS_obj > 0:
+                if TSS_obj is not None and TSS_obj > 0:
                     output_feature["TSS_norm"] = round(pathway_result.TSS_concentration / TSS_obj, 2)
-                if BOD5_obj > 0:
+                if BOD5_obj is not None and BOD5_obj > 0:
                     output_feature["BOD5_norm"] = round(pathway_result.BOD5_concentration / BOD5_obj, 2)
-                if COD_obj > 0:
+                if COD_obj is not None and COD_obj > 0:
                     output_feature["COD_norm"] = round(pathway_result.COD_concentration / COD_obj, 2)
                 if TKN_obj is not None and TKN_obj > 0:
                     output_feature["TKN_norm"] = round(pathway_result.TKN_concentration / TKN_obj, 2)
