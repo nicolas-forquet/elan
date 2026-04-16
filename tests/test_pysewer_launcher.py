361f6ed30a6f2120b5331181577904ae76244cd7
Sophie Aubier <sophie.aubier@oslandia.com>
2026-04-16 16:57:25 +0200

    add placeholders to the yaml

--- tests/test_pysewer_launcher.py
+++ tests/test_pysewer_launcher.py
@@ -4,6 +4,7 @@ test_sewer_network
 
 # pylint: disable=import-outside-toplevel, invalid-name
 
+import re
 from unittest.mock import patch
 
 import pytest
@@ -19,10 +20,21 @@ def test_main(tmp_path):
     test_data_dir_sewer = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "sewer_network"
 
     test_data_dir = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "pysewer_launcher"
-    filename_yaml = str(test_data_dir / "config.yaml")
+    filename_yaml = str(test_data_dir / "config.yaml.template")
     output_path = str(tmp_path / "OUTPUT_PATH.gpkg")
     output_path_error = str(tmp_path / "OUTPUT_PATH.shp")
     sinks_path = str(test_data_dir_sewer / "sewer_network_steu_input.gpkg")
+    placeholders_yml = {
+        "buildings_input_data": str(test_data_dir_sewer / "sewer_network_buildings_population_input.gpkg"),
+        "dem_file_path": str(test_data_dir_sewer / "sewer_network_mnt_input.tif"),
+        "roads_input_data": str(test_data_dir_sewer / "sewer_network_roads_input.gpkg"),
+    }
+    with open(filename_yaml) as f:
+        content = f.read()
+    for key, value in placeholders_yml.items():
+        content = content.replace(f"{{{{{key}}}}}", value)
+    tmp_yaml = tmp_path / "config.yaml"
+    tmp_yaml.write_text(content)
 
     # with check_install
     with patch(
@@ -30,7 +42,7 @@ def test_main(tmp_path):
         [
             "pysewer_launcher",
             "--yaml",
-            str(filename_yaml),
+            str(tmp_yaml),
             "--check-installed",
             "--output-path",
             str(output_path),
@@ -45,7 +57,7 @@ def test_main(tmp_path):
         [
             "pysewer_launcher",
             "--yaml",
-            str(filename_yaml),
+            str(tmp_yaml),
             "--output-path",
             str(output_path),
         ],
@@ -58,7 +70,7 @@ def test_main(tmp_path):
         [
             "pysewer_launcher",
             "--yaml",
-            str(filename_yaml),
+            str(tmp_yaml),
         ],
     ):
         with pytest.raises(ValueError):
@@ -70,7 +82,7 @@ def test_main(tmp_path):
         [
             "pysewer_launcher",
             "--yaml",
-            str(filename_yaml),
+            str(tmp_yaml),
             "--output-path",
             str(output_path_error),
         ],
@@ -95,7 +107,7 @@ def test_main(tmp_path):
         [
             "pysewer_launcher",
             "--yaml",
-            str(filename_yaml),
+            str(tmp_yaml),
             "--output-path",
             str(output_path),
             "--sinks-path",
@@ -111,10 +123,22 @@ def test_run(tmp_path):
 
     from ELAN.resources.pysewer import pysewer_launcher
 
-    test_data_dir_sewer = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "sewer_network"
-
     test_data_dir = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "pysewer_launcher"
-    filename_yaml = str(test_data_dir / "config.yaml")
+    test_data_dir_sewer = DIR_PLUGIN_ROOT.parent / "tests" / "data_test" / "sewer_network"
     output_path = str(tmp_path / "OUTPUT_PATH.gpkg")
     sinks_path = str(test_data_dir_sewer / "sewer_network_steu_input.gpkg")
-    pysewer_launcher.run(filename_yaml, output_path, sinks_path)
+    filename_yaml = str(test_data_dir / "config.yaml.template")
+
+    placeholders_yml = {
+        "buildings_input_data": str(test_data_dir_sewer / "sewer_network_buildings_population_input.gpkg"),
+        "dem_file_path": str(test_data_dir_sewer / "sewer_network_mnt_input.tif"),
+        "roads_input_data": str(test_data_dir_sewer / "sewer_network_roads_input.gpkg"),
+    }
+    with open(filename_yaml) as f:
+        content = f.read()
+    for key, value in placeholders_yml.items():
+        content = content.replace(f"{{{{{key}}}}}", value)
+    tmp_yaml = tmp_path / "config_template.yaml"
+    tmp_yaml.write_text(content)
+
+    pysewer_launcher.run(tmp_yaml, output_path, sinks_path)
