778aa78a1c5c23cf2c386483c6ec74bcdadc823f
Jacky Volpes <jacky.volpes@oslandia.com>
2026-08-21 12:36:27 +0200

    tests: clarify conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@
 Fixtures for Elan testing
 """
 
+# function names don't begin with test_ here, they are fixtures
+# pylint: disable=invalid-name
+
+
 import site
 
 import pytest
@@ -9,25 +13,33 @@ import pytest
 from ELAN.utils.dependencies_utils import EXTERNAL_LIRBARIES_DIR
 
 
-@pytest.fixture(scope="session", autouse=True)
-def set_external_lib_path():
+@pytest.fixture(autouse=True)
+def ignore_tr_logs(mocker):
     """
-    This is a fixture to have external libraries available during tests, if they are
-    present in EXTERNAL_LIRBARIES_DIR.
+    This is a fixture to avoid translating message logs to ensure messages
+    comparisons during tests.
+    """
+
+    mocker.patch("ELAN.utils.tr.PlgLogger")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def external_libs():
+    """
+    This is a fixture to have external libraries available during tests.
     """
 
     site.addsitedir(str(EXTERNAL_LIRBARIES_DIR))
 
 
-@pytest.fixture(scope="function")
-def elan_processing(mocker, qgis_processing):
+@pytest.fixture
+def elan_processing(mocker, qgis_processing):  # pylint: disable=unused-argument
     """
     This is a fixture to do things that prepare testing Elan processings.
 
     Returns the processing module to call the run method
     """
 
-    import processing
+    import processing  # pylint: disable=import-outside-toplevel
 
-    mocker.patch("ELAN.utils.tr.PlgLogger")  # don't care about logging anything from translations
     return processing
