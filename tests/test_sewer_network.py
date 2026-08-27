7c96154fbcfdd21daebebb6ceea8d3f08834e42e
Sophie Aubier <sophie.aubier@oslandia.com>
2026-08-27 09:22:22 +0000

    review @jvolpes

--- tests/test_sewer_network.py
+++ tests/test_sewer_network.py
@@ -213,7 +213,7 @@ def test_error_wwtp_outside_dem(elan_processing):
 
 def test_error_buildings_outside_dem(elan_processing):
     """
-    Test with an input buildings layer without the specified population field.
+    Test with an input buildings layer with buildings outside the DEM.
     The processing must fail.
     """
 
